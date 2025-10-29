7df1b116f4d28c786403e2437d940a2941c5c775
AlexFirica <alex.firica1977@gmail.com>
2025-10-29 20:30:04 +0200

    Player movement

--- spacewar.py
+++ spacewar.py
@@ -6,9 +6,54 @@ import turtle
 turtle.fd(0)
 turtle.speed(0)
 turtle.bgcolor("black")
+turtle.ht()
+turtle.setundobuffer(1) #saves memory
+turtle.tracer(1) #Speeds up drawing
 
+class Sprite(turtle.Turtle):
+  def __init__(self, spriteshape, color, startx, starty):
+    turtle.Turtle.__init__(self, shape = spriteshape)
+    self.speed(0) #animation speed
+    self.penup()
+    self.color(color)
+    self.fd(0)
+    self.goto(startx, starty)
+    self.speed = 1
 
+  def move(self):
+    self.fd(self.speed)
 
+class Player(Sprite):
+  def __init__(self, spriteshape, color, startx, starty):
+    Sprite.__init__(self, spriteshape, color, startx, starty)
+    self.speed=4
+    self.lives=3
 
+  def turn_left(self):
+    self.lt(45)
 
-delay = input("Press enter to finish")
+  def turn_right(self):
+   self.rt(45)
+
+  def accelerate(self):
+    self.speed +=1
+
+  def decelerate(self):
+    self.speed -=1
+
+#Create my sprites
+player = Player("triangle" ,"white", 0,0)
+
+#Creating keybinds
+turtle.onkey(player.turn_left, "Left")
+turtle.onkey(player.turn_right, "Right")
+turtle.onkey(player.accelerate, "Up")
+turtle.onkey(player.decelerate, "Down")
+turtle.listen()
+
+#Main game loop
+while True:
+  player.move()
+  
+
+delay = input("Press enter to finish. >")
