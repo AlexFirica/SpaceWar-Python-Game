f5541197788ea79cb84fba2a6d8516d796d729c0
AlexFirica <alex.firica1977@gmail.com>
2025-10-29 20:58:38 +0200

    Border

--- spacewar.py
+++ spacewar.py
@@ -23,6 +23,20 @@ class Sprite(turtle.Turtle):
   def move(self):
     self.fd(self.speed)
 
+    #Boundary detection
+    if self.xcor() > 290:
+      self.setx(290)
+      self.rt(60)
+    if self.xcor() < -290:
+      self.setx(-290)
+      self.rt(60)
+    if self.ycor() > 290:
+      self.sety(290)
+      self.rt(60)
+    if self.ycor() < -290:
+      self.sety(-290)
+      self.rt(60)
+
 class Player(Sprite):
   def __init__(self, spriteshape, color, startx, starty):
     Sprite.__init__(self, spriteshape, color, startx, starty)
@@ -40,6 +54,35 @@ class Player(Sprite):
 
   def decelerate(self):
     self.speed -=1
+
+class Game():
+  def __init__(self):
+    self.level = 1 
+    self.score = 0
+    self.state = "playing"
+    self.pen = turtle.Turtle()
+    self.lives = 3
+  
+  def draw_border(self):
+    #Draw border
+    self.pen.speed(0)
+    self.pen.color("white")
+    self.pen.pensize(3) 
+    self.pen.penup()
+    self.pen.goto(-300,300)
+    self.pen.pendown()
+    for side in range(4):
+      self.pen.fd(600)
+      self.pen.rt(90)
+    self.pen.penup()
+    self.pen.ht()
+
+#Create Game Objects
+game=Game()
+
+#Draw The Game Border
+game.draw_border()
+
 
 #Create my sprites
 player = Player("triangle" ,"white", 0,0)
