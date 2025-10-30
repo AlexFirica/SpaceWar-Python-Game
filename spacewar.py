a93a2a6bcddd3ff0eead4b854b3f3445734c7993
AlexFirica <alex.firica1977@gmail.com>
2025-10-30 19:24:21 +0200

    enemy/collision

--- spacewar.py
+++ spacewar.py
@@ -36,6 +36,15 @@ class Sprite(turtle.Turtle):
     if self.ycor() < -290:
       self.sety(-290)
       self.rt(60)
+  def is_collision(self, other):
+    if (self.xcor()>=(other.xcor()-20)) and \
+    (self.xcor()<=(other.xcor()+20)) and \
+    (self.ycor()>=(other.ycor()-20)) and \
+    (self.ycor()<=(other.ycor()+20)):
+      return True
+    else:
+      return False
+
 
 class Player(Sprite):
   def __init__(self, spriteshape, color, startx, starty):
@@ -54,6 +63,13 @@ class Player(Sprite):
 
   def decelerate(self):
     self.speed -=1
+
+class Enemy(Sprite):
+  def __init__(self, spriteshape, color, startx, starty):
+    Sprite.__init__(self, spriteshape, color, startx, starty)
+    self.speed = 6
+    self.setheading(random.randint(0,360))
+
 
 class Game():
   def __init__(self):
@@ -86,6 +102,7 @@ game.draw_border()
 
 #Create my sprites
 player = Player("triangle" ,"white", 0,0)
+enemy = Enemy("circle", "blue", -100, 0)
 
 #Creating keybinds
 turtle.onkey(player.turn_left, "Left")
@@ -97,6 +114,12 @@ turtle.listen()
 #Main game loop
 while True:
   player.move()
-  
+  enemy.move()
+
+  #Check for collision
+  if player.is_collision(enemy):
+    x=random.randint(-250,250)
+    y=random.randint(-250,250)
+    enemy.goto(x,y)
 
 delay = input("Press enter to finish. >")
